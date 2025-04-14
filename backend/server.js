53756158b5bd190dffa3dade8dbb3b44485b003c
Vaibhav Chauhan <104888876+Vaibhav0911@users.noreply.github.com>
2025-04-15 01:58:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://commuterfrontend.vercel.app", 
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
